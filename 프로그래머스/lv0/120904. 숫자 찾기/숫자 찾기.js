2b6f87a425823f72cf6a592ad2691a8007dbfdec
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-04-06 10:10:45 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,9 +1,7 @@
 function solution(num, k) {
-    let arr = num.toString().split('')
-    let kstr = k.toString()
-  for (let i = 0; i < arr.length; i++) {
-      
-      if(arr[i] === kstr) {
+    let str = num.toString()
+  for (let i = 0; i < str.length; i++) {
+      if(str[i] === k.toString()) {
           return i+1
       } 
   } return -1
