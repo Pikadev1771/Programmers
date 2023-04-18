b92ceb4036536d7872e0ac2a2474146e3323f4b7
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-04-18 10:34:15 +0900

    [level 1] Title: 시저 암호, Time: 5.72 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,22 +1,65 @@
+// function solution(s, n) {
+//     let upperArr = Array.from({length: 26}, (_, i) => String.fromCharCode(i + 65))
+//     let lowerArr = upperArr.map((el) => el.toLowerCase())
+    
+//     //ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    
+//     let result = ''
+//     for(let i = 0; i < s.length; i++) {
+//         if(upperArr.indexOf(s[i]) + n > 25) { // Z
+//             result += upperArr[upperArr.indexOf(s[i]) + n - 26] 
+//         } else if(lowerArr.indexOf(s[i]) + n > 25) { // z
+//             result += lowerArr[lowerArr.indexOf(s[i]) + n - 26] 
+//         } else if (upperArr.indexOf(s[i]) !== -1) { // 대문자
+//             result += upperArr[upperArr.indexOf(s[i])+n]
+//         } else if (lowerArr.indexOf(s[i]) !== -1) { //  소문자
+//             result += lowerArr[lowerArr.indexOf(s[i])+n]
+//         } else if (s[i] === ' ') {
+//             result += ' '
+//         }
+//     } return result
+// }
+
 function solution(s, n) {
     let upperArr = Array.from({length: 26}, (_, i) => String.fromCharCode(i + 65))
-    let lowerrArr = upperArr.map((el) => el.toLowerCase())
+    let lowerArr = upperArr.map((el) => el.toLowerCase())
+    let answer = ''
     
-    //ABCDEFGHIJKLMNOPQRSTUVWXYZ
-    
-    let result = ''
-    for(let i = 0; i < s.length; i++) {
-        // z 또는 Z
-        if(upperArr.indexOf(s[i]) + n > 25) { // Z
-            result += upperArr[upperArr.indexOf(s[i]) + n - 26] 
-        } else if(lowerrArr.indexOf(s[i]) + n > 25) { // z
-            result += lowerrArr[lowerrArr.indexOf(s[i]) + n - 26] 
-        } else if (upperArr.indexOf(s[i]) !== -1) { // 대문자
-            result += upperArr[upperArr.indexOf(s[i])+n]
-        } else if (lowerrArr.indexOf(s[i]) !== -1) { //  소문자
-            result += lowerrArr[lowerrArr.indexOf(s[i])+n]
-        } else if (s[i] === ' ') {
-            result += ' '
+    for (let i = 0; i < s.length; i++) {
+        let text = s[i]      
+        if(text === ' ') {
+            answer += ' '
+            continue;
+        } 
+        
+        let textArr = upperArr.includes(text) ? upperArr : lowerArr
+        let idx = textArr.indexOf(s[i])+n
+        
+        if(idx > 25) {
+            idx = idx - 25 - 1
         }
-    } return result
+        
+        answer += textArr[idx]
+        
+    }
+     return answer;
 }
+
+// function solution(s, n) {
+//  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     var lower = "abcdefghijklmnopqrstuvwxyz";
+//     var answer= '';
+
+//     for(var i =0; i <s.length; i++){
+//         var text = s[i];
+//         if(text == ' ') {
+//             answer += ' '; 
+//             continue;
+//         }
+//         var textArr = upper.includes(text) ? upper : lower;
+//         var index = textArr.indexOf(text)+n;
+//         if(index >= textArr.length) index -= textArr.length;
+//         answer += textArr[index];
+//     }
+//     return answer;
+// }
