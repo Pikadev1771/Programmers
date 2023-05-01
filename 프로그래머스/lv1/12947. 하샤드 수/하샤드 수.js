999e3654de21a8fde2ef7b3ee49cef3337346b2b
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-05-01 09:45:53 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,21 +1,15 @@
-function arrSum(arr) {
-    if(arr.length === 0) {
-        return 0
-    }
-    const head = arr[0]
-    const tail = arr.slice(1)
-    return head + arrSum(tail)
-}
+// function arrSum(arr) {
+//     if(arr.length === 0) {
+//         return 0
+//     }
+//     const head = arr[0]
+//     const tail = arr.slice(1)
+//     return head + arrSum(tail)
+// }
 
 function solution(x) {
-let arr = x.toString().split('').map(el => Number(el))
+let sum = x.toString().split('').map(el => Number(el)).reduce((acc, cur) => acc + cur)
 
-let sum = arrSum(arr)
-
-if(x % sum === 0) {
-    return true
-} else {
-    return false
-}
+return (x % sum) ? false : true
 
 }
