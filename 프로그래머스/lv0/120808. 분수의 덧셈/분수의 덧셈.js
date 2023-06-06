2160c4ec013c1dea275480dc24a843c04fdceca2
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 00:45:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,3 +1,9 @@
+// 최대 공약수 구하는 함수
+function gcd(a, b) {
+    return a%b ?  gcd(b, a%b): b
+    
+}
+
 function solution(numer1, denom1, numer2, denom2) {
    const a = numer1 * denom2 + numer2 * denom1
    const b = denom1 * denom2
@@ -5,13 +11,13 @@ function solution(numer1, denom1, numer2, denom2) {
     
     // a와 b의 최대공약수를 구해서 각각 나눠준다
     
-    // 최대공약수 max 구하기
-    for(let i = 1; i <= a; i++) {
-        if(a%i === 0 && b%i === 0) {
-            max = i
-        }
-    }
+//     // 최대공약수 max 구하기
+//     for(let i = 1; i <= a; i++) {
+//         if(a%i === 0 && b%i === 0) {
+//             max = i
+//         }
+//     }
     
     
-    return [a/max, b/max]
+    return [a/gcd(a, b), b/gcd(a, b)]
 }
