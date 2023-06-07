e61506fd163ffb71ec1e4139d0d543c8f73cb401
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 19:30:20 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,3 +1,5 @@
 function solution(n) {
-    return ~~(Math.sqrt(n)) * ~~(Math.sqrt(n)) === n ? 1 : 2
+    // return ~~(Math.sqrt(n)) * ~~(Math.sqrt(n)) === n ? 1 : 2
+    
+    return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 }
