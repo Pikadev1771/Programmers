8baa71bb95e3708b63e68796cfc566997c93d6e1
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 13:29:39 +0900

    [level 0] Title: 배열 두배 만들기, Time: 1.71 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,4 +1,5 @@
 function solution(numbers) {
     
-    return numbers.map(number => number * 2)
+    // return numbers.map(number => number * 2)
+    return numbers.reduce((acc, cur) => [...acc, cur * 2], [])
 }
