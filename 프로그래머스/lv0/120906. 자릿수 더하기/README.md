ab79a0d0279b5e30c9ebd8f7aa6c222edc32dcb7
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 18:55:44 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/README.md
+++ 프로그래머스/lv0/120906. 자릿수 더하기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 자릿수 더하기 - 120906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120906?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120906) 
 
 ### 성능 요약
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,16 +1,5 @@
-function arrSum(arr){
-	if(arr.length === 0){
-    	return 0;
-    }
-    const head = arr[0];
-    const tail = arr.slice(1);
-    return head + arrSum(tail);
-    }
-
 function solution(n) {
-    let temp = n.toString().split('')
-    let arr = temp.map((el) => Number(el))
+  arr = [...n.toString()].map(x => Number(x))
     
-    return arrSum(arr)
-    
+return arr.reduce((acc, cur) => acc + cur)
 }
