03b8c209720990aa9d11ddf1b279c37e1512113d
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 18:28:35 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,5 +1,7 @@
 function solution(my_string, letter) {
-    const arr = Array.from(my_string)
+//     const arr = Array.from(my_string)
     
-    return arr.filter(x => x !== letter).join('')
+//     return arr.filter(x => x !== letter).join('')
+    
+    return my_string.split(letter).join('')
 }
