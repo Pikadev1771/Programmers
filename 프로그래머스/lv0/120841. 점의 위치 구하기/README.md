f6ca7dc829a3ce98f09971de9e299d111d590610
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 18:10:40 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/README.md
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 점의 위치 구하기 - 120841 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120841) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120841?language=javascript) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,3 +1,8 @@
 function solution(dot) {
-   return dot[0] > 0 && dot[1] > 0 ? 1 : dot[0] < 0 && dot[1] > 0 ? 2 : dot[0] < 0 && dot[1] < 0 ? 3 : 4
+   // return dot[0] > 0 && dot[1] > 0 ? 1 : dot[0] < 0 && dot[1] > 0 ? 2 : dot[0] < 0 && dot[1] < 0 ? 3 : 4
+    
+
+    const [num1, num2] = dot
+    
+    return num1 * num2 > 0 ? num1 > 0 ? 1 : 3 : num2 > 0 ? 2 : 4
 }
