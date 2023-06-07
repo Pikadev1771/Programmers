8d7ec73d389043d1996e31aa7bc5a7a5726d8b0c
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 16:49:05 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,22 +1,6 @@
 function solution(num_list) {
-//     var answer = [];
-//     let even = 0;
-//     let odd = 0;
+    const evens = num_list.filter(num => num % 2 === 0).length
+    const odds = num_list.length - evens
     
-//     num_list.forEach((el) => {
-//         if(el%2 === 0 ) {
-//            even++;
-//            } else {
-//                odd++
-//            }
-//     })
-    
-    
-    var answer = [0, 0] 
-    num_list.forEach((el) => {
-        answer[el%2] += 1
-    })
-    
-    return answer;
+    return [evens, odds]
 }
-
