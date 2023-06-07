3f0159c71ee294c54e3a10f210ccbe6a26e92bf2
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 18:16:46 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,13 @@
 function solution(s1, s2) {
-  let count = 0;
+//   let count = 0;
     
-    for(el of s1) {
-        if(s2.indexOf(el) !== -1) count++
-    }
+//     for(el of s1) {
+//         if(s2.indexOf(el) !== -1) count++
+//     }
     
-    return count;
+//     return count;
+    
+    const result = s1.filter(str => s2.includes(str))
+    
+    return result.length
 }
