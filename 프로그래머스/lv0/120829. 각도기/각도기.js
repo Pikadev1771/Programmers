9859b93e8a65030d10ac8aa1ece9c5f856f915ea
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 16:12:01 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,6 +1,8 @@
 function solution(angle) {
-    if(angle < 90) return 1;
-    if(angle === 90) return 2;
-    if(90 < angle && angle < 180) return 3;
-    if(angle === 180) return 4;
+    // if(angle < 90) return 1;
+    // if(angle === 90) return 2;
+    // if(90 < angle && angle < 180) return 3;
+    // if(angle === 180) return 4;
+    
+    return [0, 90, 91, 180].filter(a => angle >= a).length
 }
