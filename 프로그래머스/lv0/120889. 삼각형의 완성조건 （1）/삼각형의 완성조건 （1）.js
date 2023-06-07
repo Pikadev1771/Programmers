67c1702a9c6923df7004caf0c93f471ba3f1c6c5
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 16:02:51 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,10 +1,14 @@
 function solution(sides) {
     
-    const max = Math.max(...sides) // 가장 긴 변
+//     const max = Math.max(...sides) // 가장 긴 변
     
-    const sum = sides.reduce((acc, cur) => acc + cur)
+//     const sum = sides.reduce((acc, cur) => acc + cur)
     
-    return sum > max * 2 ? 1 : 2
+//     return sum > max * 2 ? 1 : 2
+    
+    sides.sort((a, b) => b - a)
+    
+    return sides[0] < sides[1] + sides[2] ? 1 : 2
     
     
     
