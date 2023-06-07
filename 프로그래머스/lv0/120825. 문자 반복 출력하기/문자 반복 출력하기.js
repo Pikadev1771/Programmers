51acb537412f28e52c59a8adc8e577cf723ab0d6
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-07 19:05:05 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,5 +1,9 @@
 function solution(my_string, n) {
-
-    return  [...my_string].map(el => el.repeat(n)).join('')
-
-    }
+    return [...my_string].map(x => {
+        let str = x
+        for(let i = 1; i < n; i++) {
+            str += x
+        }
+        return str
+    }).join('')
+}
